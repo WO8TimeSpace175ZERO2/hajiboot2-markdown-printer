9df495d84a6071141c25517ac3f500914089940b
WO8TimeSpace175ZERO2 <fk.yoshizaki@gmail.com>
2020-05-17 15:24:29 +0900

    初回登録

---
+++ src/main/java/hajiboot/hajiboot2markdownprinter/Hajiboot2MarkdownPrinterApplication.java
@@ -0,0 +1,13 @@
+package hajiboot.hajiboot2markdownprinter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hajiboot2MarkdownPrinterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hajiboot2MarkdownPrinterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hajiboot/hajiboot2markdownprinter/Hajiboot2MarkdownPrinterApplicationTests.java
@@ -0,0 +1,13 @@
+package hajiboot.hajiboot2markdownprinter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hajiboot2MarkdownPrinterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
