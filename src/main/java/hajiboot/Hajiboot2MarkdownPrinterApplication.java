c722b5f9221dd60acd1e2326609494ef80f16104
WO8TimeSpace175ZERO2 <fk.yoshizaki@gmail.com>
2020-05-18 22:40:30 +0900

    場所が間違っていたため修正

--- src/main/java/hajiboot/Hajiboot2MarkdownPrinterApplication.java
+++ src/main/java/hajiboot/Hajiboot2MarkdownPrinterApplication.java
@@ -1,4 +1,4 @@
-package hajiboot.hajiboot2markdownprinter;
+package hajiboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
